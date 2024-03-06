865e1350339cb927d044c8be7fdb02b664b5f7e5
3frik <86358988+3frik@users.noreply.github.com>
2024-03-06 09:18:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function sessionSave(){
+    var sessionKey = document.getElementById("sessionSaveKey").value; //find the element with the name of the variable and save it
+    var sessionValue = document.getElementById("sessionSaveValue").value; //find the element with the value os the variable and save it
+    console.log("Session Save. Key: "+sessionKey+" and value: "+sessionValue); //for testing and control
+    sessionStorage.setItem(sessionKey,sessionValue); //save the variable in the computer
+}
+
+function sessionRead(){
+    var sessionKey = document.getElementById("sessionReadKey").value; //find the element with the name of the variable and save it
+    var sessionValue = sessionStorage.getItem(sessionKey); //with the name, find the value of the variable
+    console.log("Session Read. Key: "+sessionKey+" and value: "+sessionValue); //for testing and control
+    document.getElementById("sessionReadValue").innerText=sessionValue; //Show the value on screen
+}
+
+function localSave(){
+    var localKey = document.getElementById("localSaveKey").value; //find the element with the name of the variable and save it
+    var localValue = document.getElementById("localSaveValue").value; //find the element with the value os the variable and save it
+    console.log("local Save. Key: "+localKey+" and value: "+localValue); //for testing and control
+    localStorage.setItem(localKey,localValue); //save the variable in the computer
+}
+
+function localRead(){
+    var localKey = document.getElementById("localReadKey").value; //find the element with the name of the variable and save it
+    var localValue = localStorage.getItem(localKey); //with the name, find the value of the variable
+    console.log("Local Read. Key: "+localKey+" and value: "+localValue); //for testing and control
+    document.getElementById("localReadValue").innerText=localValue; //Show the value on screen
+}
+
+function cookieSave(){
+    var cookieKey = document.getElementById("cookieSaveKey").value; //find the element with the name of the variable and save it
+    var cookieValue = document.getElementById("cookieSaveValue").value; //find the element with the value os the variable and save it
+    console.log("Cookie Save. Key: "+cookieKey+" and value: "+cookieValue); //for testing and control
+    document.cookie= cookieKey+"="+cookieValue+";"+";path=/"; //save the variable in the computer
+}
+
+function cookieRead(){
+    var cookieKey = document.getElementById("cookieReadKey").value; //find the element with the name of the variable and save it
+    var cookieValue = document.cookie.cookieKey; //with the name, find the value of the variable
+    console.log("Cookie Read. Key: "+cookieKey+" and value: "+cookieValue); //for testing and control
+    document.getElementById("CookieReadValue").innerText=cookieValue; //Show the value on screen
+}
